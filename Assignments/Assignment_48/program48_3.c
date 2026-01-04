466ac65acae343ef94939109af167853726470ad
Niscahy Hemant Deshmukh <nischaydeshmukh@gmail.com>
2026-01-04 21:33:09 +0530

    Add files via upload

---
+++ Assignments/Assignment_48/program48_3.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE; 
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)  // LL is empty
+    {
+        *first = newn;
+    }
+    else                // LL contains atleast 1 node
+    {
+        newn->next = (*first);
+        (*first) = newn;
+    }
+}
+
+void MultiplyByTwo(PNODE first)
+{
+    PNODE temp = first;
+
+    while(temp != NULL)
+    {
+        temp->data = temp->data * 2;
+        temp = temp -> next;
+    }
+    
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d |->",first->data);
+        first = first -> next;
+    }
+    printf("NULL \n");
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0 , iValue = 0 ;
+
+    InsertFirst(&head,75);
+    InsertFirst(&head,51);
+    InsertFirst(&head,22);
+    InsertFirst(&head,11);
+
+    Display(head);
+
+    MultiplyByTwo(head);
+
+    Display(head);
+    
+    return 0;
+}
